c826bfd8bd0263ffc0ce7d8f8c795d460cf35b52
Moti Zilberman <motiz88@gmail.com>
2015-12-21 20:44:15 +0200

    Remove Bluebird & coffee-script as runtime dependencies. This sets up the package to be published to npm as plain JS.

--- index.js
+++ index.js
@@ -2,12 +2,8 @@
 /* jshint -W097 */
 'use strict';
 
-
-var coffee = require('coffee-script');
-coffee.register();
-
-module.exports = require('./lib/promiseFtp');
+module.exports = require('./dist/promiseFtp');
 module.exports.FtpConnectionError = require('promise-ftp-common').FtpConnectionError;
 module.exports.FtpReconnectError = require('promise-ftp-common').FtpReconnectError;
 module.exports.STATUSES = require('promise-ftp-common').STATUSES;
-module.exports.ERROR_CODES = require('./lib/errorCodes');
+module.exports.ERROR_CODES = require('./dist/errorCodes');
